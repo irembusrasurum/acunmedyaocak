c9e53d50412efcc426493c46ec373ab2fa5eeb81
irembusrasurum <146779362+irembusrasurum@users.noreply.github.com>
2025-03-17 20:31:47 +0300

    Add files via upload

---
+++ acunmedyaocak3.hafta.py
@@ -0,0 +1,35 @@
+
+for i in range(1, 101):
+    print(i, end=" ")
+print("\n")
+
+
+
+for i in range(2, 101, 2):
+    print(i, end=" ")
+print("\n")
+
+
+
+sayi = int(input("Faktöriyelini hesaplamak için bir sayı girin: "))
+faktoriyel = 1
+for i in range(1, sayi + 1):
+    faktoriyel *= i
+print(f"{sayi}! = {faktoriyel}")
+
+
+
+
+
+def asal_mi(n):
+    if n < 2:
+        return Falses
+    for a in range(2, int(n**0.5) + 1):  
+        if n % a == 0:
+            return False
+    return True
+
+print("1'den 100'e kadar olan asal sayılar:")
+for i in range(1, 101):
+    if asal_mi(i):
+        print(i, end=" ")
